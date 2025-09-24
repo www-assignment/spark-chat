a215a698caaf82df5125178fbaa642dd4930e24d
emmanuelifiok50 <emmanuelifiok50@gmail.com>
2025-09-24 22:28:28 +0100

    Fix deployment

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Remove all the export and path configurations
-  // Let Vercel handle the deployment automatically
-}
-
-module.exports = nextConfig
